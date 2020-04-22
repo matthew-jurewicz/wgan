cadc788350e5e933255cb8142adffd837f40270e
Matthew Jurewicz <matthewjurewicz@Matthews-MacBook-Air.local>
2020-04-22 11:14:14 -0400

    Fix preview

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ rand = random.sample(range(len(data)), k=4)
 _, ax = plt.subplots(2, 2)
 for i in range(4):
     x, _ = data[rand[i]]
-    ax[i // 2][i % 2].imshow(x.numpy().transpose(1, 2, 0))
+    ax[i // 2][i % 2].imshow(x.permute(1, 2, 0))
 plt.show()
 
 data = torch.utils.data.DataLoader(
@@ -34,6 +34,8 @@ model = torch.hub.load(
     model='DCGAN', 
     pretrained=False, 
     useGPU=torch.cuda.is_available(), 
+    dimG=256, 
+    dimD=256, 
     lambdaGP=10
 )
 
@@ -43,8 +45,8 @@ for i in range(10):
         loss = model.optimizeParameters(x)
 
     z, _ = model.buildNoiseData(n_samples=1)
-    ax.imshow(model.test(z)[0]
-                    .numpy().transpose(1, 2, 0))
+    out = 255 * (model.test(z)[0] + 1) / 2
+    ax.imshow(out.type(torch.uint8).permute(1, 2, 0))
     fig.canvas.draw()
 
 model.save('model.pt')
@@ -52,6 +54,6 @@ model.save('model.pt')
 model.load('model.pt')
 
 z, _ = model.buildNoiseData(n_samples=1)
-plt.imshow(model.test(z)[0]
-                .numpy().transpose(1, 2, 0))
+out = 255 * (model.test(z)[0] + 1) / 2
+plt.imshow(out.type(torch.uint8).permute(1, 2, 0))
 plt.show()
